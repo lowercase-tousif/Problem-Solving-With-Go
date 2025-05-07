1aab45ca6fad32384ebc88451faaa1b933325a40
Tousif Tasrik <83619372+lowercase-tousif@users.noreply.github.com>
2025-05-07 13:43:32 +0600

    Add leetcode problem no 162. Find peek element added

---
+++ 162. Find Peak Element/main.go
@@ -0,0 +1,17 @@
+func findPeakElement(nums []int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+	    mid := low + (high-low)/2
+
+		if mid > 0 && nums[mid] < nums[mid-1] {
+			high = mid - 1
+		} else if mid < len(nums)-1 && nums[mid] < nums[mid+1] {
+			low = mid + 1
+		}else{
+            return mid
+        }
+	}
+    return -1
+}
